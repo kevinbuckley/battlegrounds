d7451f82b3b038a0f8dfe41a179449bd36d7ac4a
Kevin Buckley <kbuckley17@gmail.com>
2026-04-25 13:40:29 -0400

    feat: Add taunt keyword support and test minion

---
+++ src/game/minions/tier1/taunt-minion.ts
@@ -0,0 +1,12 @@
+import { defineMinion } from "../define";
+
+export default defineMinion({
+  id: "taunt_minion",
+  name: "Taunt Minion",
+  tier: 1,
+  tribes: [],
+  baseAtk: 1,
+  baseHp: 1,
+  baseKeywords: ["taunt"],
+  hooks: {},
+});
